022b1eeb752a4bdbb9ec3c217a0062c410f4582d
qianqiancui <49955758+qianqiancui@users.noreply.github.com>
2022-03-21 12:04:40 -0700

    Add files via upload

--- task.js
+++ task.js
@@ -95,17 +95,13 @@ var race_pages = [// add as a list as many pages as you like
 
 var rand_sex_key = Math.random();
 
-if (rand_sex_key <= 0.5) {
-    var sex_key_assig = "f-female";
-} else if (rand_sex_key > 0.5) {
-    var sex_key_assig = "f-male";
-}
+var sex_key_assig;
 
 // randomize block order
 var rand_block = Math.random();
 if (rand_block < .5) {
     var block_order = "1_sex_2_race";
-} else if (rand_block >= .5){
+} else {
     var block_order = "1_race_2_sex";
 }
 
@@ -226,8 +222,12 @@ var show_stim = function (image) {
 
     //}, 800);
 
-//    if (rand_sex_key <= 0.5) {
-    if (sex_key_assig === "f-female"){
+    if (rand_sex_key <= 0.5) {
+        sex_key_assig = "f-female";
+
+        //document.getElementById('female').innerHTML = "Female";
+        //document.getElementById('male').innerHTML = "&nbsp;&nbsp;Male&nbsp;&nbsp;";
+
 
         //d3.select("#male").text("&nbsp;&nbsp;Male&nbsp;&nbsp;");
         d3.select("#female").text("Female")
@@ -236,7 +236,8 @@ var show_stim = function (image) {
         //$("#female").text("Female");
 
 
-    } else if (sex_key_assig === "f-male") {
+    } else {
+        sex_key_assig = "f-male";
 
 
         d3.select("#male").text("Female");
@@ -1054,22 +1055,18 @@ $(window).load(function () {
                 // a list of pages you want to display in sequence
                 general_instruction_pages.concat( sex_pages_f_female),
 
-                function () { currentview = new sex_block();
-                                   console.log(sex_key_assig);
-                                   } // what you want to do when you are done with instructions
+                function () { currentview = new sex_block(); } // what you want to do when you are done with instructions
             );
         }
-        else if (sex_key_assig === "f-male"){
+        else {
             psiTurk.doInstructions(
                 general_instruction_pages.concat(sex_pages_f_male),
-                function () { currentview = new sex_block();
-                                   console.log(sex_key_assig);
-                                   } // what you want to do when you are done with instructions
+                function () { currentview = new sex_block(); } // what you want to do when you are done with instructions
             );
         }
 
       
-        } else if (block_order === "1_race_2_sex") {
+        } else {
 
             psiTurk.doInstructions(
                 // a list of pages you want to display in sequence
